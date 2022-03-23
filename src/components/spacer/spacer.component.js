0f55afb496a98189210c4c46819610772ee285c5
samLynux <samuelbisnes@gmail.com>
2022-03-23 16:34:12 +0700

    7-44

---
+++ src/components/spacer/spacer.component.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import {  StatusBar, StyleSheet, Text, View, SafeAreaView, Platform } from 'react-native';
+
+import styled, {useTheme} from "styled-components/native"
+
+
+const sizeVariant = {
+    small: 1,
+    medium: 2,
+    large: 3,
+}
+
+const positionVariant = {
+    top: "marginTop",
+    left: "marginLeft",
+    right: "marginRight",
+    bottom: "marginBottom",
+}
+
+const getVariant = (position, size, theme) => {
+    const sizeIndex = sizeVariant[size]
+    const property = positionVariant[position]
+    const value = theme.space[sizeIndex]
+    return `${property}:${value}`
+}
+const SpacerView = styled.View`
+    ${({variant}) => variant}
+`
+
+export const Spacer = ({position, size, children}) => {
+    const theme = useTheme();
+    const variant = getVariant(position, size, theme)
+    return(
+        <SpacerView variant={variant}>
+            {children}
+        </SpacerView>
+    )
+}
+
+
+Spacer.defaultProps = {
+    position: "top",
+    size : "small"
+}
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Platform } from 'react-native';
+import { StyleSheet, Text, Image, View,  Platform } from 'react-native';
 import { Card, Searchbar } from 'react-native-paper';
 import styled from "styled-components/native"
-import { View } from "react-native-web";
 import { SvgXml } from "react-native-svg";
 import star from "../../../../assets/star"
 import open from "../../../../assets/open"
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 const RestaurantCard = styled(Card)`
     background-color:${props => props.theme.colors.bg.primary}
@@ -56,14 +56,14 @@ const Open = styled(SvgXml)`
 export const RestaurantInfoCard = ({restaurant = {}}) => {
     const {
         name ='some res', 
-        icon, 
+        icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png", 
         photos=[
             'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'
         ], 
         address='100 res street', 
         isOpenNow=true, 
         rating=4, 
-        isClosedTemporary=false, 
+        isClosedTemporary=true, 
     } = restaurant;
 
     const ratingArray =Array.from(new Array(Math.floor(rating)));
@@ -82,7 +82,16 @@ export const RestaurantInfoCard = ({restaurant = {}}) => {
                         ))} 
                     </Rating>
                     <SectionEnd>
+                        {isClosedTemporary && (
+                            <Text variant="label" style={{color:"red"}}>
+                                CLOSED TEMPORARILY
+                            </Text>
+                        )}
+                        <Spacer position="left" size="large"/>
                         {isOpenNow && <Open xml={open} width={20} height={20}/>}
+                        <Spacer position="left" size="large"/>
+                            <Image style={{width:15, height:15 }} source={{uri: icon}}/>
+                        
                     </SectionEnd>
                 </Section>
                 <Address>{address}</Address>
